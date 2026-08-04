839f69044ae59e0ea04f2d0fc08d63ba81f6b51d
Ninulina <nina.oulovska@gmail.com>
2026-08-04 18:33:18 +0200

    Initialize application and set today's date
    Added initial application setup and date initialization.

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
+/* ---------------------------------
+   Nákup 2.1
+   Základní funkce aplikace
+----------------------------------*/
+
+
+// načtení uložených nákupů
+let nakupy = JSON.parse(
+    localStorage.getItem("nakupy")
+) || [];
+
+
+// testovací zpráva
+console.log("Nákup 2.1 spuštěn");
+
+
+// dnešní datum
+const datum = document.getElementById("datum");
+
+if (datum) {
+    datum.value = new Date()
+        .toISOString()
+        .split("T")[0];
+}
 
